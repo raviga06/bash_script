c4084e1f575ed9c01abbba0ec03d292e2423ccca
Ravikanth <s.ravikanth2009@gmail.com>
2024-03-20 19:10:51 +0530

    Adding new script

---
+++ name.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#ask the user for their name
+echo "what is you name?"
+read name
+
+#greet the user
+
+echo "Hello, $name! Nice to meet you"
+    #!/bin/bash
+    # Ask the user for their name
+    echo “What’s your name?”
+    read name
+    # Greet the user
+    echo “Hello, $name! Nice to meet you.”
+
