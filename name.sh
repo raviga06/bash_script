a5372b87c47eebca71362a1a27457ffcd40a62b4
Ravikanth <s.ravikanth2009@gmail.com>
2024-03-20 19:26:29 +0530

    added new code

--- name.sh
+++ name.sh
@@ -6,10 +6,4 @@ read name
 #greet the user
 
 echo "Hello, $name! Nice to meet you"
-    #!/bin/bash
-    # Ask the user for their name
-    echo “What’s your name?”
-    read name
-    # Greet the user
-    echo “Hello, $name! Nice to meet you.”
 
